449d0a659918259720b93a22636079f5976ce56d
Jorge681jorge <gutierrez.jorge681@gmail.com>
2021-12-04 21:57:15 -0500

    cover
    datasource, resolvers and typedefs

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,9 +1,9 @@
 //Se importan los typeDef de cada submodulo
 
 const userTypeDef = require("./user_type_def");
-
+const coverTypeDef = require("./cover_type_def");
 //Se agregan los typeDefs importados para exportarlos
-const schemasArrays = [userTypeDef];
+const schemasArrays = [userTypeDef, coverTypeDef];
 
 //Se exportan
 module.exports = schemasArrays;
